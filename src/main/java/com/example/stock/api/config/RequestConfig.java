0691b70409fdab797d02e15e0f52a9424f057048
Ebuka.Maduekwe <ebuka.maduekwe@interswitchng.com>
2021-02-23 11:13:01 +0100

    context part config

---
+++ src/main/java/com/example/stock/api/config/RequestConfig.java
@@ -0,0 +1,31 @@
+package com.example.stock.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.WebFilter;
+
+@Configuration
+public class RequestConfig {
+
+    private final String contextPath;
+
+    public RequestConfig(@Value("${server.servlet.context-path}")String contextPath) {
+        this.contextPath = contextPath;
+    }
+
+    @Bean
+    public WebFilter contextPathWebFilter() {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            if (request.getURI().getPath().startsWith(contextPath)) {
+                return chain.filter(
+                        exchange.mutate()
+                                .request(request.mutate().contextPath(contextPath).build())
+                                .build());
+            }
+            return chain.filter(exchange);
+        };
+    }
+}
